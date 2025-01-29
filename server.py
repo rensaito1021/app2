f54fd160ff628569935968759e707073a431c031
rensaito1021 <157346153+rensaito1021@users.noreply.github.com>
2025-01-29 09:21:08 +0000

    変更

--- server.py
+++ server.py
@@ -101,5 +101,6 @@ def read_root():
 
 # FastAPIアプリケーションを起動
 if __name__ == "__main__":
-    uvicorn.run(app, host="0.0.0.0", port=8000)
+    uvicorn.run(app, host="0.0.0.0", port=8000)  #uvicorn server:app --host 0.0.0.0 --reload
+
 
